598f3e1e9005bcdecdcf8000d66eefe98f159f5b
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-12-24 15:48:26 -0200

    inserting controller functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-
+import router from "./lib/api/expenses/index";
 const app = express();
 const port = "5000";
 
+app.use(router);
 app.listen(port, () => console.info(`App listening at port ${port}`));
