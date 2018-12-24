0dd915b2b88eee611fb0d3fe95664dc13e08b478
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-12-24 18:17:15 -0200

    Update Crud

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
-import router from "./lib/api/expenses/index";
+import router from "./lib/api/expenses";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = "5000";
 
 app.use(router);
+app.use(bodyParser);
 app.listen(port, () => console.info(`App listening at port ${port}`));
